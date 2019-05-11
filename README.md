89ffe9485adbf9f57941d5cce758459911fed2d6
auravadima <auravadima@gmail.com>
2019-05-11 16:33:42 +0700

    Задача 5

--- README.md
+++ README.md
@@ -8,11 +8,15 @@
 компиляция  
 javac LenOfFirstLine.java  
 запуск  
-java LenOfFirstLine LenOfFirstLine
+java LenOfFirstLine {filename}
 
 # Необходимые тесты (Задача 3)
 [Тесты](https://vk.com/away.php?utf=1&to=https%3A%2F%2Fdocs.google.com%2Fdocument%2Fd%2F1EOaN3E9vd44aCZcZR1lG88JpQNR-OJv4iAWKD7Q8TJo%2Fedit%3Fusp%3Dsharing "Тесты")
 
 # Задача 4
-Проект с тестами в папке testapp.
-Для реализации тестов были использованы рекомендованные инструменты.
+Проект с тестами в папке testapp.  
+Для реализации тестов были использованы Java, Maven, JUnit, Rest Assured.
+
+# Задача 5
+Проект с тестами в папке autoapp.  
+Для реализации тестов были использованы Java, Maven, JUnit, Selenium WebDriver.
---
+++ autoapp/src/main/java/Utils.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+class Utils {
+    static WebDriver createChromeDriverAtRasp(final String from, final String to, final String when, boolean onlyBuses){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rasp.yandex.ru");
+
+        WebElement fromInput = driver.findElement(By.name("fromName"));
+        fromInput.clear();
+        fromInput.sendKeys(from);
+
+        WebElement toInput = driver.findElement((By.name("toName")));
+        toInput.sendKeys(to);
+
+        WebElement dateInput = driver.findElement(By.id("when"));
+        dateInput.sendKeys(when);
+
+        if(onlyBuses){
+            WebElement busRadioButton = driver.findElement(By.cssSelector(".RadioButton:nth-child(5) > .RadioButton__buttonLable"));
+            busRadioButton.click();
+        }
+
+        dateInput.submit();
+
+        return driver;
+    }
+}
